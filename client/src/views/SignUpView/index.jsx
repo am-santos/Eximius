493cc1e6cc3f11b0ec4e518be4a94ddee8e67330
Ana de Almeida <ana.isabel190@gmail.com>
2020-06-08 12:14:43 +0100

    small update

---
+++ client/src/views/SignUpView/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function SignUpView() {
+  return (
+    <div>
+      <form action="">
+        <label htmlFor="username-input"></label>
+        <input type="text" id="username-input" />
+        <label htmlFor=""></label>
+        <input type="text"/>
+        <label htmlFor=""></label>
+        <input type="text"/>
+      </form>
+    </div>
+  )
+}
+
+export default SignUpView

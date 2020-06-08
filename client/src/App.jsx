586857e9892aabb1f803921be459afdc679bfdb6
am-santos <antopuppe@gmail.com>
2020-06-08 09:47:00 +0100

    created routes client side

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,38 @@
 import React from 'react';
 
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+
 import './App.css';
+
+import HomeView from './views/HomeView';
 
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        {/* LogoBar or NavBar */}
+
+        <Switch>
+          {/* Events /> */}
+          <Route path='/' component={HomeView} />
+          <Route path='/event/:id' component={EventSingleView} />
+          <Route path='/my-events' component={MyEventListView} />
+          <Route path='/event/create' component={CreateEventView} />
+          <Route path='/event/:id/edit' component={EventEditView} />
+
+          {/* Authentication /> */}
+          <Route path='/authentication/log-in' component={LogInView} />
+          <Route path='/authentication/sign-up/:token' component={SignUpView} />
+
+          {/* Profile /> */}
+          <Route path='/profile/sign-up' component={ProfileView} />
+          <Route path='/profile/edit' component={EditProfileView} />
+
+          {/* Contact Us /> */}
+          <Route path='/contact-us' component={ContactUsView} />
+          {/* <Route path='/contact-us/edit' component={EditProfileView} /> */}
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }

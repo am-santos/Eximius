3cba487f0110d82acaa1174a3dea5f91d5d7687b
am-santos <antopuppe@gmail.com>
2020-06-12 12:29:47 +0100

    added random title

--- client/src/components/AnimationTitle/index.jsx
+++ client/src/components/AnimationTitle/index.jsx
@@ -4,50 +4,83 @@ export class AnimationTitle extends Component {
   constructor() {
     super();
     this.state = {
-      title: ''
-    }
+      title: '',
+      counter: 0
+    };
   }
 
   componentDidMount() {
-    this.timerID = setTimeout(() => setInterval(this.animationTitle(), 1000), 1000)
-  }
-
-  // componentWillUnmount() {
-    
-  // }
-
-  animationTitle = () => {
-    setTimeout(() => setInterval(animation, 1000), 1000)
-    const letterArr = ['E','X','I','M','Y','U','Q','M','P'];
-    const titleLength = document.getElementsByClassName('title').length;
-    const animation = () =>{
-      for (let i=0; i< titleLength; i++) {
-        document.getElementsByClassName('title').innerText = letterArr[Math.floor(Math.random() * titleLength)]
-        document.getElementsByClassName('title').innerText= letterArr[Math.floor(Math.random() * titleLength)]
-        document.getElementsByClassName('title').innerText = letterArr[Math.floor(Math.random() * titleLength)]
-        document.getElementsByClassName('title').innerText = letterArr[Math.floor(Math.random() * titleLength)]
-        document.getElementsByClassName('title').innerText = letterArr[Math.floor(Math.random() * titleLength)]
-        document.getElementsByClassName('title').innerText = letterArr[Math.floor(Math.random() * titleLength)]
-        document.getElementsByClassName('title').innerText = letterArr[Math.floor(Math.random() * titleLength)]
-        i++
-      }
+    if (this.state.counter < 10) {
+      this.timerID = setTimeout(
+        setInterval(() => this.animationTitle(), 500),
+        3000
+      );
     }
   }
 
-  copyPasteFunction = () => {
+  animationTitle = () => {
+    const letterArr = [
+      'E',
+      'X',
+      'I',
+      'M',
+      'S',
+      'S',
+      'S',
+      'U',
+      'E',
+      'X',
+      'I',
+      'M',
+      'I',
+      'U',
+      'E',
+      'X',
+      'I',
+      'M',
+      'U',
+      'E',
+      'X',
+      'I',
+      'M',
+      'U'
+    ];
+    const title = 'EXIMIUS';
+    let randomTitle = '';
+    let tempRandom = '';
+    let tempCounter = this.state.counter;
+    for (let i = 0; i < title.length; i++) {
+      // tempRandom = title.slice(0 + 1, i + 1);
+      randomTitle += letterArr[Math.floor(Math.random() * letterArr.length)];
+      tempCounter++;
+    }
+    if (tempCounter < 50) {
+      this.setState({
+        title: randomTitle,
+        counter: tempCounter
+      });
+    } else {
+      this.setState({
+        title: 'EXIMIUS'
+      });
+    }
+  };
 
+  componentWillUnmount() {
+    clearInterval(this.timerID);
   }
-  
+
   render() {
     return (
-      <h1 onLoad={this.copyPasteFunction}>
-        <div className="title">E</div>
-        <div className="title">x</div>
-        <div className="title">i</div>
-        <div className="title">m</div>
-        <div className="title">i</div>
-        <div className="title">u</div>
-        <div className="title">s</div>
+      <h1>
+        {this.state.title}
+        {/* <div className='title'>E</div>
+        <div className='title'>x</div>
+        <div className='title'>i</div>
+        <div className='title'>m</div>
+        <div className='title'>i</div>
+        <div className='title'>u</div>
+        <div className='title'>s</div> */}
       </h1>
     );
   }
